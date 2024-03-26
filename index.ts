540681fff53b89d32f20dc0b47475ff1f09d9a37
Asra Urooj <160761127+Asraurooj@users.noreply.github.com>
2024-03-26 11:54:45 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node 
+ 
+ import inquirer from "inquirer";
+
+ //Printing a Wellcome Message
+ console.log("\n\tWellcome To \"Code With Asra\"  -CLI Simple Calculator\n");
+
+//Asking Questions from users through Inquirer
+
+let answers = await inquirer.prompt([
+     {message: "Enter First Number", type: "number", name: "firstNumber"},
+     {message: "Enter Second Number", type: "number", name: "secondNumber"},
+     {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division",],
+     },
+]);
+
+// conditional statements If.Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
+
